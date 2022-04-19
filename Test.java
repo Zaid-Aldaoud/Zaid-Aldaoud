1821bb11a84374ee213c8219dc4bf48f651b9862
Zaid Daoud <zaidaliciouss@gmail.com>
2022-04-19 14:32:11 +0300

    program

---
+++ Test.java
@@ -0,0 +1,26 @@
+
+public class Test {
+
+	
+	
+		
+		
+		
+		public static void main(String[] args) {
+			
+			int arr[] = {7,2,9,4,99,6,27,8,15,10};
+			int index = 10;
+			
+	
+	
+	for(int i=0;i<index;i++) {
+		
+		System.out.println("index for i = "+arr[i]);
+		
+	}
+		
+		}
+}
+	
+	
+
